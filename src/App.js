3d0dec0db81bca9841f7b56ea4433c0ce25d90e6
Wolrang <dkfud252@naver.com>
2023-03-29 16:24:29 +0900

    🎉 Wesely Common

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='a'>안녕하세요 Wesely입니다.</p>
+      <p className='b'>안녕하세요 Wesely입니다.</p>
+      <p className='c'>안녕하세요 Wesely입니다.</p>
+      <p className='d'>안녕하세요 Wesely입니다.</p>
+    </div>
+  );
+}
+
+export default App;
